dec4b33a8228b8ba32f7facade2faefecd2e05ea
DESKTOP-FFB7FEU\gerge <gergely.glasz@bedag.ch>
2024-02-28 10:13:38 +0100

    Add code

---
+++ FunctionApp2/Program.cs
@@ -0,0 +1,24 @@
+using FunctionAppNet8;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public static class Program
+{
+    public static void Main()
+    {
+        //var dummy = new Dummy();
+        //dummy.Display("TEST");
+
+        var host = new HostBuilder()
+            .ConfigureFunctionsWebApplication()
+            .ConfigureServices(services =>
+            {
+                services.AddApplicationInsightsTelemetryWorkerService();
+                services.ConfigureFunctionsApplicationInsights();
+            })
+            .Build();
+
+        host.Run();
+    }
+}
---
+++ FunctionAppNet8/Dummy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionAppNet8
+{
+    public class Dummy
+    {
+        public void Display(string message)
+        {
+            System.Diagnostics.Trace.Write(message);
+        }
+    }
+}
